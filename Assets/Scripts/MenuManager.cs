d70aec2b349cb0b836a3fdfc2c3528cc06ba8e38
John Haley <jah2809@rit.edu>
2025-02-21 05:44:34 -0500

    Main Menu Basics with Fading

---
+++ Assets/Scripts/MenuManager.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MenuManager : MonoBehaviour
+{
+    [Header("Menu Fading")]
+    [SerializeField] bool waitToLerp = true;
+    [SerializeField] float menuAlphaThreshold = 0.2f;
+    [SerializeField] float fadeSpeed = 1.0f;
+
+
+    private bool canLerp = false;
+    
+    private CanvasGroup from, to;
+
+    private void Start() {
+    }
+
+    private void Update()
+    {
+        // Fading Menus
+        if (canLerp)
+        {
+            from.alpha -= Time.deltaTime * fadeSpeed;
+            if(from.alpha <= menuAlphaThreshold && waitToLerp || !waitToLerp)
+            {
+                to.gameObject.SetActive(true);
+                to.alpha += Time.deltaTime * fadeSpeed;
+            }
+
+            if (from.alpha == 0)
+            {
+                from.gameObject.SetActive(false);
+            }
+        };
+    }
+
+    // Fading Menu Functions
+    public void SetFrom(CanvasGroup From) => from = From;
+
+    public void SetTo(CanvasGroup To)
+    {
+        to = To;
+        to.alpha = 0;
+    } 
+    public void ChangeMenu() => canLerp = true;
+
+    // Scene Managment
+
+    public void ChangeSceneFromIndex(int sceneIndex) => SceneManager.LoadScene(sceneIndex); 
+    public void ChangeSceneFromName(string sceneName) => SceneManager.LoadScene(sceneName); 
+    public void Quit() => Application.Quit();
+}
