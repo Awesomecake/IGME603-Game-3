889100b0ca9f6206de4548f4e385dd0172c8c7d1
Luke Justice <justicelu97@gmail.com>
2025-02-26 15:57:57 -0500

    adding support for a delayed state change in the state machine

---
+++ Assets/Scripts/Entity/StateMachine/Generic/Transition/DelayedTriggerStateTransition.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using UnityEngine;
+
+public class DelayedTriggerStateTransition : TriggeredStateTransition
+{
+    [SerializeField] private float delaySeconds;
+    private Coroutine _delay;
+
+    protected override void Transition()
+    {
+        if (_delay != null) return;
+        _delay = StartCoroutine(Delay());
+    }
+
+    public void CancelTransition()
+    {
+        StopCoroutine(_delay);
+        _delay = null;
+    }
+
+    private IEnumerator Delay()
+    {
+        yield return new WaitForSeconds(delaySeconds);
+        base.Transition();
+        yield return null;
+    }
+
+    public override bool NeedsTransition()
+    {
+        if (StateMachine.GetCurrentState() != fromState)
+        {
+            CancelTransition();
+        }
+
+        return false;
+    }
+}
