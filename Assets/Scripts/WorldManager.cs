561894ab8e0fe61322a7e98f641ff328b8d02f8d
Luke Justice <justicelu97@gmail.com>
2025-03-03 14:00:28 -0500

    made a prefab out of the world manager so its easier to drag and drop in

--- Assets/Scripts/WorldManager.cs
+++ Assets/Scripts/WorldManager.cs
@@ -14,18 +14,19 @@ public class WorldManager : MonoBehaviour
             Destroy(gameObject);
             return;
         }
+
         Instance = this;
     }
 
     public Tilemap world;
-    public GoalDiamond diamond;
+    [HideInInspector] public GoalDiamond diamond;
 
     public TileBase GetTile(Vector3 worldPosition)
     {
         var cell = world.WorldToCell(worldPosition);
         return world.GetTile(cell);
     }
-    
+
     public List<Vector3> FindPath(Vector3 start, Vector3 end)
     {
         var path = new List<Vector3>();
@@ -38,7 +39,7 @@ public class WorldManager : MonoBehaviour
         if (world.GetTile(startTile) != null) return path;
         if (world.GetTile(endTile) != null) return path;
         Debug.Log($"Finding path from {startTile} to {endTile}");
-        
+
         return AStar.Search(startTile, endTile, AStar.CrossProduct)
             .Select(it => world.CellToWorld(it) + new Vector3(0.5f, 0.5f))
             .ToList();
