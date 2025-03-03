75c1c89ffaa3abe8279ba5c103b2a7f81b8b83d6
Luke Justice <justicelu97@gmail.com>
2025-03-03 13:23:22 -0500

    fixing some issues where enemy would continue to detect downed bro

--- Assets/Scripts/Entity/Enemy/StateMachine/State/StunnedState.cs
+++ Assets/Scripts/Entity/Enemy/StateMachine/State/StunnedState.cs
@@ -6,6 +6,7 @@ public class StunnedState : State
 
     [SerializeField] private GameObject stunnedIndicatorPrefab;
     [SerializeField] private Vector3 offset = Vector3.up;
+    [SerializeField] private Collider2D visionArea;
     private GameObject _stunnedIndicator;
 
     private void Start()
@@ -18,10 +19,12 @@ public class StunnedState : State
     {
         character.SetState(Enemy.State.Stunned);
         EnableIndicator(true);
+        if (!visionArea) visionArea.enabled = false;
     }
 
     public override void ExitState()
     {
+        if (visionArea) visionArea.enabled = true;
         character.SetState(Enemy.State.Normal);
         EnableIndicator(false);
     }
--- Assets/Scripts/Entity/LilBro/StateMachine/DownedStateMachine.cs
+++ Assets/Scripts/Entity/LilBro/StateMachine/DownedStateMachine.cs
@@ -3,10 +3,19 @@ using UnityEngine;
 public class DownedStateMachine : HierarchicalStateMachine
 {
     [SerializeField] private LilBro self;
+
     public override void EnterState()
     {
         base.EnterState();
         self.currentState = LilBro.State.Downed;
+        var broCollider = self.GetComponent<Collider2D>();
+        
+        if (!broCollider) return;
+        broCollider.enabled = false;
+        StartCoroutine(Util.AfterDelay(
+            0.1f,
+            () => broCollider.enabled = true
+        ));
     }
 
     public override void ExitState()
