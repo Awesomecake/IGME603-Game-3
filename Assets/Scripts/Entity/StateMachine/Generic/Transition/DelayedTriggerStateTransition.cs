880f6b5fcee70e01de81f9614d47c354d0124b70
Luke Justice <justicelu97@gmail.com>
2025-03-03 13:49:00 -0500

    fixed an issue where the guard wouldnt properly reset patrol after being stunned

--- Assets/Scripts/Entity/StateMachine/Generic/Transition/DelayedTriggerStateTransition.cs
+++ Assets/Scripts/Entity/StateMachine/Generic/Transition/DelayedTriggerStateTransition.cs
@@ -3,18 +3,37 @@ using UnityEngine;
 
 public class DelayedTriggerStateTransition : TriggeredStateTransition
 {
+    public enum DuplicateInvocationMode
+    {
+        ContinuePrevious,
+        ResetTimer
+    }
+
+    [SerializeField] private DuplicateInvocationMode mode = DuplicateInvocationMode.ResetTimer;
+
     [SerializeField] private float delaySeconds;
     private Coroutine _delay;
 
     protected override void Transition()
     {
-        if (_delay != null) CancelTransition();
+        switch (mode)
+        {
+            case DuplicateInvocationMode.ContinuePrevious:
+                if (_delay != null) return;
+                break;
+            
+            case DuplicateInvocationMode.ResetTimer:
+            default:
+                CancelTransition();
+                break;
+        }
+
         _delay = StartCoroutine(Delay());
     }
 
     public void CancelTransition()
     {
-        StopCoroutine(_delay);
+        if (_delay != null) StopCoroutine(_delay);
         _delay = null;
     }
 
