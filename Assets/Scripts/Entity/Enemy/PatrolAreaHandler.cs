58db437b3c87508afe85a36018cbb98bf24fe2ef
Luke Justice <justicelu97@gmail.com>
2025-03-06 14:22:09 -0500

    making enemies wander into walls less frequently

--- Assets/Scripts/Entity/Enemy/PatrolAreaHandler.cs
+++ Assets/Scripts/Entity/Enemy/PatrolAreaHandler.cs
@@ -6,7 +6,7 @@ public class PatrolAreaHandler : MonoBehaviour
     [SerializeField] private PatrolArea areaPrefab;
 
     private PatrolArea _area;
-    
+
     private void Start()
     {
         _area = Instantiate(areaPrefab, transform.position, Quaternion.identity);
@@ -14,6 +14,19 @@ public class PatrolAreaHandler : MonoBehaviour
 
     public void NextPoint()
     {
-        patrolTarget.UpdatePosition(_area.GetRandomLocation());
+        var randomLocation = _area.GetRandomLocation();
+        const int maxRetries = 20;
+        var retryCount = 0;
+        while (
+            retryCount < maxRetries &&
+            WorldManager.Instance &&
+            WorldManager.Instance.GetTile(randomLocation)
+        )
+        {
+            randomLocation = _area.GetRandomLocation();
+            retryCount++;
+        }
+
+        patrolTarget.UpdatePosition(randomLocation);
     }
 }
