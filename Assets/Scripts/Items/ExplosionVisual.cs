9aae7b1ba22a73c516cb648d411b9ea89b8f44a4
Luke Justice <justicelu97@gmail.com>
2025-03-09 19:19:54 -0400

    grenade explosion is now lethal

--- Assets/Scripts/Items/ExplosionVisual.cs
+++ Assets/Scripts/Items/ExplosionVisual.cs
@@ -1,19 +1,39 @@
+using System;
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class ExplosionVisual : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private bool isLethal;
+
+    private Collider2D _collider2D;
+    
+    private void Start()
     {
+        _collider2D = GetComponent<Collider2D>();
         StartCoroutine(DestroyDelay());
     }
 
-    //wait to destroy visual after a second
     private IEnumerator DestroyDelay()
     {
-        yield return new WaitForSeconds(0.5f);
+        yield return new WaitForSeconds(0.05f);
+        _collider2D.enabled = false;
+        yield return new WaitForSeconds(0.45f);
         Destroy(gameObject);
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (!other.CompareTag("Player")) return;
+        if (LevelManager.Instance)
+        {
+            LevelManager.Instance.NotifyPlayerDie();
+        }
+        else
+        {
+            Destroy(other.gameObject);
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
     }
 }
