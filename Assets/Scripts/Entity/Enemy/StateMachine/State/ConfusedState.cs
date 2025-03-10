f889c82ece1de8ebf886d7281544e9de8d595bc7
FMPO <chrissante3.14@gmail.com>
2025-03-09 21:00:19 -0400

    removed faulty effect spawning

--- Assets/Scripts/Entity/Enemy/StateMachine/State/ConfusedState.cs
+++ Assets/Scripts/Entity/Enemy/StateMachine/State/ConfusedState.cs
@@ -8,30 +8,21 @@ public class ConfusedState : State
     [SerializeField] private Vector3 offset = Vector3.up;
     [SerializeField] private float rotationRange = 15f;
     [SerializeField] private float rotationPeriod = 2f;
-    private GameObject _confusedIndicator;
 
     private float _enterTime;
     private float _baseRotation;
 
-
-    private void Start()
-    {
-        _confusedIndicator = Instantiate(confusedIndicatorPrefab, character.transform);
-        EnableIndicator(false);
-    }
     public override void EnterState()
     {
         character.body.velocity = Vector3.zero;
 
         _baseRotation = character.transform.rotation.eulerAngles.z;
         _enterTime = Time.time;
-        EnableIndicator(true);
     }
 
     public override void ExitState()
     {
         character.SetState(Enemy.State.Normal);
-        EnableIndicator(false);
     }
 
     public override void StateUpdate()
@@ -42,9 +33,5 @@ public class ConfusedState : State
         character.transform.rotation = Quaternion.Euler(0f, 0f, _baseRotation + currentOffset);
     }
 
-    private void EnableIndicator(bool isEnabled)
-    {
-        _confusedIndicator.transform.position = character.transform.position + offset;
-        _confusedIndicator?.SetActive(isEnabled);
-    }
+  
 }
